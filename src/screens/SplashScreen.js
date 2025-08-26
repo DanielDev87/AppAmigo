21fef92e665c2ef2892ed3bf08b908eca390db9b
DanielDev87 <danielhymtj@icloud.com>
2025-08-26 06:45:28 -0500

    Se guarda el screen splash

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,10 +1,32 @@
-import { View } from "react-native"
+import { LinearGradient } from "expo-linear-gradient";
+import { Text, Image, StyleSheet } from "react-native"
+import colors from "../constants/colors";
 
 const SplashScreen =()=> {
     return(
-        <View>
-            <Text>Loading...</Text>
-            <Image source={require('../../assets/splash-icon.png')} />
-        </View>
+        <LinearGradient colors={colors.gradientePrimario} style={styles.container}>
+            <Text style={styles.text}>Loading...</Text>
+            <Image source={require('../../assets/splash-icon.png')} style={styles.logo} />
+        </LinearGradient>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        color: colors.luminous,
+        marginBottom: 20,
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+    },
+});
+
+export default SplashScreen;
