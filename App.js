799d414ec737914dafc36d2cb6d68730d0d50964
DanielDev87 <danielhymtj@icloud.com>
2025-10-21 07:38:27 -0500

    Inicializando el servicio y envolviendo el app con el proveedor

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import AppProvider from './navigation/AppProvider';
+
 
 export default function App() {
   return (
+    <AppProvider>
     <NavigationContainer>
       <AppNavigator />
       <StatusBar style="auto" />
     </NavigationContainer>
+    </AppProvider>
   );
 }
 
